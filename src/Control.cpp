9e81c5aaed9cfc47d02a230f65429cb37f60a943
Bendeguz-Cs <csermendy.bendeguz@gmail.com>
2025-02-13 15:48:28 +0100

    Update Control.cpp

--- src/Control.cpp
+++ src/Control.cpp
@@ -1,11 +1,11 @@
 /*
-  Control.cpp - Library for control devices.
+  Timing_control.cpp - Library for control devices.
   Created by Csermendy Bendeguz, February 13, 2025.
 */
 
 #include "Arduino.h"
 
-Control::Control(int CLK_PIN, int DT_PIN)
+Control::Control()
 {
   
 }
