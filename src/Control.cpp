667916c53a7269a4f305085d760dcd8f55ec909b
Bendeguz-Cs <csermendy.bendeguz@gmail.com>
2025-02-13 14:47:13 +0100

    Update Control.cpp

--- src/Control.cpp
+++ src/Control.cpp
@@ -1,1 +1,21 @@
+/*
+  Control.cpp - Library for control devices.
+  Created by Csermendy Bendeguz, February 13, 2025.
+*/
 
+#include "Arduino.h"
+
+Control::Control(int CLK_PIN, int DT_PIN)
+{
+  
+}
+
+void Control::()
+{
+    
+}
+
+long Control::()
+{
+
+}
