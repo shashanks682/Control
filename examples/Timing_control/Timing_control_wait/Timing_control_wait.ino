76f217b6ecb95256d89d8eaf306559749619284a
Bendeguz-Cs <csermendy.bendeguz@gmail.com>
2025-02-14 11:40:16 +0100

    Update Timing_control_wait.ino

--- examples/Timing_control/Timing_control_wait/Timing_control_wait.ino
+++ examples/Timing_control/Timing_control_wait/Timing_control_wait.ino
@@ -1,1 +1,15 @@
+#include <Timing_control.h>
 
+const int pin = 2; // Define your pin here
+const unsigned long delayTime = 1000; // Define your delay time in milliseconds
+
+Timing_control timingControl(pin, delayTime);
+
+void setup() {
+    Serial.begin(9600); // Initialize serial communication
+}
+
+void loop() {
+    bool result = timingControl.wait();
+    Serial.println(result); // Write the returned value to the serial monitor
+}
